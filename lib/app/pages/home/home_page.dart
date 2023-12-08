5f6d2dc5710da099b1988e0de4786e27a2682455
Gabrielle-Demberck <gabrielle.demberck@outlook.com>
2023-12-08 01:51:39 -0300

    wip

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -7,24 +7,76 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          AnimatedTextKit(
-            animatedTexts: [
-              TypewriterAnimatedText('Gabrielle Demberck',
-                  textStyle: const TextStyle(
-                    fontSize: 42.0,
-                    fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.only(left: 15),
+        child: Wrap(
+          children: [
+            ConstrainedBox(
+              constraints: BoxConstraints.loose(const Size.fromWidth(370)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Gabrielle Demberck',
+                    style: TextStyle(
+                        fontSize: 54.0,
+                        fontWeight: FontWeight.bold,
+                        height: 0.8),
                   ),
-                  speed: const Duration(milliseconds: 300),
-                  cursor: '|'),
-            ],
-            repeatForever: false,
-            isRepeatingAnimation: false,
-            displayFullTextOnTap: true,
-            stopPauseOnTap: true,
-          )
-        ],
+                  SizedBox(
+                    height: 100,
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        TypewriterAnimatedText('FullStack Developer| Mobile',
+                            textStyle: const TextStyle(
+                              fontSize: 34.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            speed: const Duration(milliseconds: 200),
+                            cursor: '|'),
+                      ],
+                      repeatForever: true,
+                      totalRepeatCount: 1,
+                      isRepeatingAnimation: true,
+                      displayFullTextOnTap: true,
+                      stopPauseOnTap: true,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                    child: Row(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'About Me',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 50,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Contact',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 500,
+              color: Colors.amber,
+            )
+          ],
+        ),
       ),
     );
   }
